dc87ed5bd35ad4a2ed8186dc78629fa767a30ef8
junki3737 <amour1125.j.0609@gmail.com>
2022-08-07 01:29:18 +0900

    initial commit

---
+++ StockList/src/main/java/com/example/stocklist/StockListApplication.java
@@ -0,0 +1,12 @@
+package com.example.stocklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockListApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StockListApplication.class, args);
+	}
+
+}
